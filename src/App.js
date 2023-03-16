3ae60a8fe9ce0aa069eac010be7a8a02153d5da3
AyjayJr <ayjayjr@hotmail.com>
2023-03-16 14:41:09 -0400

    created auth context that manages all the authentication states, makes app comoponent leaner and keeps all authentication logic in one place

--- src/App.js
+++ src/App.js
@@ -1,52 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
-import AuthContext from './context/auth-context';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './context/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // use effect takes an arrow function and a list of dependencies
-  // will only run on app reload if the set of dependencies has changed
-  // here the list of dependencies is empty so this will only run on app startup
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-
-    if (storedUserLoggedInInformation === '1') {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-
-    // this creates a key value pair stored in browser local storage
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
-
+  const ctx = useContext(AuthContext)
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler
-        }}
-    >
-      {/* must wrap all the components that plan to use a context with a .Provider */}
-      <MainHeader onLogout={logoutHandler} />
+    <React.Fragment>
+      <MainHeader onLogout={ctx.logoutHandler} />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
