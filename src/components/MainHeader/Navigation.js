5b0b3f2734ca269c7347017cddbf7538617db9b4
AyjayJr <ayjayjr@hotmail.com>
2023-03-16 14:16:33 -0400

    replaced context consumer with context hook in Navigation.js

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,36 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../context/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  // using context hook instead of .Consumer
+  // syntax is a lot cleaner, i prefer this method
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {/* .Consumer takes a function that returns the JSX code that needs access to the context */}
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
